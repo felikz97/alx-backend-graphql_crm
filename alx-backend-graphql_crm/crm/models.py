5accac5171593fa6eaa8c4db1f84fae5dfdb4561
USER <felikz.kipkemoi@gmail.com>
2025-06-29 20:46:36 +0300

    0. Set Up GraphQL Endpoint

---
+++ alx-backend-graphql_crm/crm/models.py
@@ -0,0 +1,23 @@
+# crm/models.py
+from django.db import models
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    order_date = models.DateTimeField(auto_now_add=True)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    def save(self, *args, **kwargs):
+        if not self.total_amount:
+            self.total_amount = sum(product.price for product in self.products.all())
+        super().save(*args, **kwargs)
+        
--- crm/graphql_crm/schema.py
+++ crm/graphql_crm/schema.py
@@ -1,10 +1,9 @@
 import graphene
-from crm.schema import Query as CRMQuery, Mutation as CRMMutation
 
-class Query(CRMQuery, graphene.ObjectType):
-    pass
+class Query(graphene.ObjectType):
+    hello = graphene.String()
 
-class Mutation(CRMMutation, graphene.ObjectType):
-    pass
+    def resolve_hello(root, info):
+        return "Hello, GraphQL!"
 
-schema = graphene.Schema(query=Query, mutation=Mutation)
+schema = graphene.Schema(query=Query)
