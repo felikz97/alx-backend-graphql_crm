068350c9b32f6ceeebcffa184227e8d542ba4e0a
Felix Kipkemoi <felikz.kipkemoi@gmail.com>
2025-07-13 17:33:45 +0300

    celery update

--- alx-backend-graphql_crm/alx-backend-graphql_crm/crm/celery.py
+++ alx-backend-graphql_crm/alx-backend-graphql_crm/crm/celery.py
@@ -2,7 +2,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_backend_graphql_crm.settings')
 
 app = Celery('alx_backend_graphql_crm')
