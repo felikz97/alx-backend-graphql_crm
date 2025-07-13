f7ffef6006386a12df741395e34f9e577d56409b
Felix Kipkemoi <felikz.kipkemoi@gmail.com>
2025-07-13 16:30:28 +0300

    3. Schedule a GraphQL Mutation for Product Stock Alerts

--- crm/cron.py
+++ crm/cron.py
@@ -4,12 +4,16 @@ from gql import gql, Client
 from datetime import datetime
 import requests
 
+# Set up transport and client for GraphQL requests
+transport = RequestsHTTPTransport(url='http://localhost:8000/graphql', verify=False, retries=3)
+client = Client(transport=transport, auto_schema=True)
+
 def log_crm_heartbeat():
     timestamp = datetime.now().strftime('%d/%m/%Y-%H:%M:%S')
     message = f"{timestamp} CRM is alive"
     try:
-        response = requests.post('http://localhost:8000/graphql', json={'query': '{ hello }'})
-        if response.status_code == 200 and 'data' in response.json():
+        response = client.execute(gql('{ hello }'))
+        if response and 'data' in response:
             message += " - GraphQL OK"
         else:
             message += " - GraphQL failed"
@@ -18,4 +22,22 @@ def log_crm_heartbeat():
 
     with open('/tmp/crm_heartbeat_log.txt', 'a') as log:
         log.write(message + "\n")
-        
+
+def update_low_stock():
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    try:
+        response = requests.post('http://localhost:8000/graphql', json={
+            'query': 'mutation { updateLowStockProducts { products { name stock } message } }'
+        })
+        if response.status_code == 200:
+            data = response.json()
+            lines = [f"{timestamp} - {p['name']} restocked to {p['stock']}" for p in data['data']['updateLowStockProducts']['products']]
+            with open('/tmp/low_stock_updates_log.txt', 'a') as f:
+                f.write("\n".join(lines) + "\n")
+        else:
+            raise Exception("Bad response")
+    except Exception as e:
+        with open('/tmp/low_stock_updates_log.txt', 'a') as f:
+            f.write(f"{timestamp} - Error: {e}\n")
+    print("Low stock update processed!")
+    return "Low stock update processed!"
--- crm/graphql_crm/schema.py
+++ crm/graphql_crm/schema.py
@@ -9,3 +9,22 @@ class Mutation(CRMMutation, graphene.ObjectType):
     pass
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
+class UpdateLowStockProducts(graphene.Mutation):
+    class Output:
+        products = graphene.List(ProductType)
+        message = graphene.String()
+
+    def mutate(self, info):
+        low_stock = Product.objects.filter(stock__lt=10)
+        for p in low_stock:
+            p.stock += 10
+            p.save()
+        return UpdateLowStockProducts(products=low_stock, message="Stock levels updated for low-stock products")
+
+class Mutation(graphene.ObjectType):
+    create_customer = CreateCustomer.Field()
+    bulk_create_customers = BulkCreateCustomers.Field()
+    create_product = CreateProduct.Field()
+    create_order = CreateOrder.Field()
+    update_low_stock_products = UpdateLowStockProducts.Field()
+    
