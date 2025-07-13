660b6633e76b78bd8c98ca900ec106ff8d8c9687
felikz97 <felikz.kipkemoi@gmail.com>
2025-07-13 21:27:22 +0300

    update celery

--- celery.py
+++ celery.py
@@ -5,6 +5,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_backend_graphql_crm.settings')
 
-app = Celery('alx_backend_graphql_crm')
+app = Celery('crm')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
