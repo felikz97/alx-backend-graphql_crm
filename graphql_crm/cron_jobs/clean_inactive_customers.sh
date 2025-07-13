09f7c85f9bf61625ef0206b56f585bd9e202b3df
Felix Kipkemoi <felikz.kipkemoi@gmail.com>
2025-07-13 13:15:34 +0300

    Schedule a Customer Cleanup Script

---
+++ graphql_crm/cron_jobs/clean_inactive_customers.sh
@@ -0,0 +1,16 @@
+# crm/cron_jobs/clean_inactive_customers.sh
+#!/bin/bash
+
+# Run Django shell command to delete inactive customers and log the result
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+DELETED=$(python manage.py shell <<EOF
+from crm.models import Customer, Order
+from datetime import datetime, timedelta
+
+a_year_ago = datetime.now() - timedelta(days=365)
+cust_ids = Customer.objects.exclude(order__order_date__gte=a_year_ago).distinct().values_list("id", flat=True)
+deleted_count, _ = Customer.objects.filter(id__in=cust_ids).delete()
+print(deleted_count)
+EOF
+)
+echo "$TIMESTAMP - Deleted $DELETED inactive customers" >> /tmp/customer_cleanup_log.txt
